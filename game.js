f548d541b409c21e2d3f97a323fc7f97607ef3b1
vigism <rushilvig@gmail.com>
2019-06-15 12:10:04 -0700

    Update ball position at interval

--- game.js
+++ game.js
@@ -1,15 +1,23 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20,40,50,50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dX = 2;
+var dY = -2;
+function draw() {
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    drawBall();
+    x += dX;
+    y +=  dY;
+}
 
-ctx.beginPath();
-ctx.arc(240,160,20,0,Math.PI*2,false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x,y,10,0,Math.PI*2);
+    ctx.fillStyle="#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
+setInterval(draw,10);

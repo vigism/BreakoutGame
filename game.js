d3886652335c490f14b30a9605f32ba2e205132e
vigism <rushilvig@gmail.com>
2019-06-15 12:14:39 -0700

    Add wall physics

--- game.js
+++ game.js
@@ -5,16 +5,27 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dX = 2;
 var dY = -2;
+var ballRadius = 10;
+
 function draw() {
     ctx.clearRect(0,0,canvas.width,canvas.height)
     drawBall();
+
+    if(y + dY > canvas.height-ballRadius || y + dY < ballRadius){
+        dY = -dY;
+    }
+
+    if(x + dX > canvas.width-ballRadius || x + dX < ballRadius){
+        dX = -dX;
+    }
     x += dX;
     y +=  dY;
+    
 }
 
 function drawBall(){
     ctx.beginPath();
-    ctx.arc(x,y,10,0,Math.PI*2);
+    ctx.arc(x,y,ballRadius,0,Math.PI*2);
     ctx.fillStyle="#0095DD";
     ctx.fill();
     ctx.closePath();

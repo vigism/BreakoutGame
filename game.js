1e6a4c36cbc42400466dba1f388ab9e17210bc4b
vigism <rushilvig@gmail.com>
2019-06-15 12:27:41 -0700

    Add paddle functionality

--- game.js
+++ game.js
@@ -1,14 +1,27 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+//canvas variables
 var x = canvas.width/2;
 var y = canvas.height-30;
+
+//ball variables
 var dX = 2;
 var dY = -2;
 var ballRadius = 10;
 
+//paddle variables
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+//control variables
+var rightPressed = false;
+var leftPressed = false;
+
 function draw() {
     ctx.clearRect(0,0,canvas.width,canvas.height)
+    //ball logic
     drawBall();
 
     if(y + dY > canvas.height-ballRadius || y + dY < ballRadius){
@@ -20,7 +33,14 @@ function draw() {
     }
     x += dX;
     y +=  dY;
-    
+
+    //paddle logic
+    if(rightPressed && paddleX < canvas.width-paddleWidth){
+        paddleX += 7;
+    }else if (leftPressed && paddleX > 0){
+        paddleX -= 7;
+    }
+    drawPaddle();
 }
 
 function drawBall(){
@@ -29,6 +49,34 @@ function drawBall(){
     ctx.fillStyle="#0095DD";
     ctx.fill();
     ctx.closePath();
+}
+
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth,paddleHeight);
+    ctx.fillStyle="#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+//listeners
+document.addEventListener("keydown",keyDownHandler, false);
+document.addEventListener("keyup",keyUpHandler, false);
+
+function keyDownHandler(e){
+    if(e.key == "Right" || e.key == "ArrowRight"){
+        rightPressed = true;
+    } else if (e.key == "Left" || e.key == "ArrowLeft"){
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e){
+    if(e.key == "Left" || e.key == "ArrowLeft"){
+        leftPressed = false;
+    }else if (e.key == "Right" || e.key == "ArrowRight"){
+        rightPressed = false;
+    }
 }
 
 setInterval(draw,10);
